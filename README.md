e1e41d091a36df4b494076ffc4b013a27cc6a43e
Charlie Gerard <charlie.a.gerard@gmail.com>
2017-04-14 21:55:49 +0100

    sending vibrate command working

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Myo Web bluetooth - WIP
 
 Connect the MYO armband to a web page using web bluetooth.
+
+
+#### Services and characteristics:
+
+* Control service = 0001;
+
+* BatteryService                = 0x180f;
+* BatteryLevelCharacteristic    = 0x2a19;
+
+* IMUDataService = 'd5060002-a904-deb9-4748-2c7f4a124842';
+* IMUDataCharacteristic = 'd5060402-a904-deb9-4748-2c7f4a124842';
--- public/index.js
+++ public/index.js
@@ -2,17 +2,18 @@
 
 window.onload = function(){
   var button = document.getElementById("connect");
+  let batteryLevelCharacteristic;
 
   button.onclick = function(e){
     return navigator.bluetooth.requestDevice({
       filters: [
         {name: "Myo"},
         {
-          //GCControlService
-          services: ['d5060001-a904-deb9-4748-2c7f4a124842']
+          //Battery Service // IMU Data Service // Control Service
+          services: [0x180f, 'd5060002-a904-deb9-4748-2c7f4a124842', 'd5060001-a904-deb9-4748-2c7f4a124842']
         }
       ],
-      optionalServices: ['d5060001-a904-deb9-4748-2c7f4a124842']
+      // optionalServices: ['battery_service']
     })
     .then(device => {
       console.log('Device discovered', device.name);
@@ -24,15 +25,34 @@ window.onload = function(){
       //GCControlService
       return server.getPrimaryService('d5060001-a904-deb9-4748-2c7f4a124842');
 
+      // Battery Service
+      // return server.getPrimaryService('battery_service');
+      // return server.getPrimaryService(0x180f);
+
+      // Test IMU data service
+      // return server.getPrimaryService('d5060002-a904-deb9-4748-2c7f4a124842');
     })
     .then(service => {
       console.log('getting in service');
-      //Command characteristic - WriteValue to indicate a device is listening
-      return service.getCharacteristic("d5060401-a904-deb9-4748-2c7f4a124842");
+      // Battery level characteristic
+      // return service.getCharacteristic('battery_level');
+      // return service.getCharacteristic(0x2a19);
+
+      // Test IMU Data characteristic
+      // return service.getCharacteristic('d5060402-a904-deb9-4748-2c7f4a124842');
+      
+      // Test Command characteristic
+      return service.getCharacteristic('d5060401-a904-deb9-4748-2c7f4a124842');
     })
     .then(characteristic => {
-      var value = new Uint8Array([0x03,0x01])
-      return characteristic.writeValue(value)
+      // batteryLevelCharacteristic = characteristic;
+      // console.log(batteryLevelCharacteristic);
+      var value = new Uint8Array([0x03,1,0x03]);
+      console.log(value);
+      characteristic.writeValue(value);
+      // console.log(characteristic.readValue());
+      //  batteryLevelCharacteristic.addEventListener('characteristicvaluechanged',
+      //      handleBatteryLevelChanged);
     })
     .then(() => {
       console.log('vibrated');
@@ -40,10 +60,33 @@ window.onload = function(){
     .catch(error => {console.log('error',error)})
   }
 
-  function handleValueChanged(event){
-    // let level = event.target.value.getUint8(0);
-    // console.log('New value is: ', level);
-    console.log('here');
-    console.log(event.target.value);
+  // function handleValueChanged(event){
+  //   // let level = event.target.value.getUint8(0);
+  //   // console.log('New value is: ', level);
+  //   console.log('here');
+  //   console.log(event.target.value);
+  // }
+
+  function handleBatteryLevelChanged(event){
+    console.log('not getting here');
+    // let batteryLevel = event.target.value.getUint8(0);
+    let imuData = event.target.value;
+    // console.log('> Battery Level is ' + batteryLevel + '%');
+    console.log('> IMU Data is ' + imuData);
   }
+
+  // var notificationsButton = document.getElementById('start');
+
+  // notificationsButton.onclick = function(){
+  //   console.log('Starting IMU Data Notifications...');
+  //   batteryLevelCharacteristic.startNotifications()
+  //   .then(_ => {
+  //     console.log('> Notifications started');
+  //     document.querySelector('#start').disabled = true;
+  //     document.querySelector('#stop').disabled = false;
+  //   })
+  //   .catch(error => {
+  //     console.log('Argh! ' + error);
+  //   });
+  // }
 }
