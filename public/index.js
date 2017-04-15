42947b08bcd4b97e9bb9016aa8f493e7bc52a909
Charlie Gerard <charlie.a.gerard@gmail.com>
2017-04-15 13:28:23 +0100

    cleaning code a little bit

--- public/index.js
+++ public/index.js
@@ -5,7 +5,6 @@ window.onload = function(){
   let batteryLevelCharacteristic;
 
   let standardServer;
-  let standardService;
 
   button.onclick = function(e){
     return navigator.bluetooth.requestDevice({
@@ -41,8 +40,8 @@ window.onload = function(){
       // return server.getPrimaryService('d5060005-a904-deb9-4748-2c7f4a124842');
     })
     .then(service => {
-      console.log('getting in service');
-      standardService = service;
+      console.log('getting service');
+
       // Battery level characteristic
       // return service.getCharacteristic('battery_level');
       // return service.getCharacteristic(0x2a19);
@@ -50,7 +49,7 @@ window.onload = function(){
       // Test IMU Data characteristic
       // return service.getCharacteristic('d5060402-a904-deb9-4748-2c7f4a124842');
 
-      // Test Command characteristic
+      // Command characteristic
       return service.getCharacteristic('d5060401-a904-deb9-4748-2c7f4a124842');
 
       //Test EMG characteristic
@@ -60,7 +59,6 @@ window.onload = function(){
       // batteryLevelCharacteristic = characteristic;
       commandCharacteristic = characteristic;
       // console.log(batteryLevelCharacteristic);
-      console.log(characteristic);
       // return characteristic.startNotifications()
 
       // var value = new Uint8Array([0x03,1,0x03]); //Array made of type of command (vibrate), 1 for activate and last argument for vibration type.
@@ -70,27 +68,25 @@ window.onload = function(){
       //return new Buffer([0x01,3,emg_mode,imu_mode,classifier_mode]);
 
       var commandValue = new Uint8Array([0x01,3,0x02,0x01,0x01]);
-      commandCharacteristic.writeValue(commandValue);
+      commandCharacteristic.writeValue(commandValue); //This tells the Myo to enable the streaming of data.
       // console.log(characteristic.readValue());
       //  batteryLevelCharacteristic.addEventListener('characteristicvaluechanged',
       //      handleBatteryLevelChanged);
     })
     .then(_ => {
       // console.log('vibrated');
-      // console.log('done', );
+
+      // Get other service: IMUData Service
       return standardServer.getPrimaryService('d5060002-a904-deb9-4748-2c7f4a124842');
-      // console.log(characteristic);
       // characteristic.addEventListener('characteristicvaluechanged', handleBatteryLevelChanged)
     })
-    .then(serv => {
-      console.log('trying to get IMU char');
-      return serv.getCharacteristic('d5060402-a904-deb9-4748-2c7f4a124842');
+    .then(newService => {
+      //Getting IMUData Characteristic
+      return newService.getCharacteristic('d5060402-a904-deb9-4748-2c7f4a124842');
     })
     .then(char => {
-      console.log('trying to get notifications');
-      char.startNotifications().then(test => {
-        char.addEventListener('characteristicvaluechanged',
-                                    handleBatteryLevelChanged);
+      char.startNotifications().then(res => {
+        char.addEventListener('characteristicvaluechanged', handleIMUDataChanged);
       })
     })
     .catch(error => {console.log('error',error)})
@@ -104,33 +100,37 @@ window.onload = function(){
   // }
 
   function handleBatteryLevelChanged(event){
-    console.log('not getting here');
     // let batteryLevel = event.target.value.getUint8(0);
-    // let imuData = event.target.value.length;
-    let imuDataX = event.target.value.getUint8(0);
+    // console.log('> Battery Level is ' + batteryLevel + '%');
+  }
+
+  function handleIMUDataChanged(event){
+    //byteLength of ImuData DataView object is 20.
+    let imuData = event.target.value;
+    // console.log(imuData.byteLength);
+    // let imuDataX = event.target.value.getUint8(0);
+    let imuDataX = event.target.value.getInt16(0);
     let imuDataY = event.target.value.getUint8(1);
     let imuDataZ = event.target.value.getUint8(2);
     let imuDataTest = event.target.value.getUint8(3);
-    // console.log('> Battery Level is ' + batteryLevel + '%');
-    // console.log('> IMU Data is ' + imuData);
-    console.log('> IMU Data is ' + imuDataX + 'Y ' + imuDataY + 'here', imuDataZ);
-    console.log(imuDataTest);
+    console.log(imuDataX / 16384);
+    // console.log('> IMU Data is ' + imuDataX + 'Y ' + imuDataY + 'here', imuDataZ);
   }
 
-  var notificationsButton = document.getElementById('start');
-
-  notificationsButton.onclick = function(){
-    console.log('Starting IMU Data Notifications...');
-    batteryLevelCharacteristic.startNotifications()
-    .then(char => {
-      batteryLevelCharacteristic.addEventListener('characteristicvaluechanged',
-                                    handleBatteryLevelChanged);
-      console.log('> Notifications started');
-      document.querySelector('#start').disabled = true;
-      document.querySelector('#stop').disabled = false;
-    })
-    .catch(error => {
-      console.log('Argh! ' + error);
-    });
-  }
+  // var notificationsButton = document.getElementById('start');
+  //
+  // notificationsButton.onclick = function(){
+  //   console.log('Starting IMU Data Notifications...');
+  //   batteryLevelCharacteristic.startNotifications()
+  //   .then(char => {
+  //     batteryLevelCharacteristic.addEventListener('characteristicvaluechanged',
+  //                                   handleBatteryLevelChanged);
+  //     console.log('> Notifications started');
+  //     document.querySelector('#start').disabled = true;
+  //     document.querySelector('#stop').disabled = false;
+  //   })
+  //   .catch(error => {
+  //     console.log('Argh! ' + error);
+  //   });
+  // }
 }
