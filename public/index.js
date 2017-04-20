5cc58a23032ebb93b3d82d51bc6e7fcc9762d4ab
Charlie Gerard <charlie.a.gerard@gmail.com>
2017-04-20 22:08:43 +0100

    refactor and tried to get pose data

--- public/index.js
+++ public/index.js
@@ -73,56 +73,6 @@ window.onload = function(){
       //Test EMG Data Service
       // return server.getPrimaryService('d5060005-a904-deb9-4748-2c7f4a124842');
     })
-    // .then(service => {
-    //   console.log('getting service');
-    //
-    //   // Battery level characteristic
-    //   // return service.getCharacteristic('battery_level');
-    //   // return service.getCharacteristic(0x2a19);
-    //
-    //   // Test IMU Data characteristic
-    //   // return service.getCharacteristic('d5060402-a904-deb9-4748-2c7f4a124842');
-    //
-    //   // Command characteristic
-    //   // return service.getCharacteristic('d5060401-a904-deb9-4748-2c7f4a124842');
-    //
-    //   //Test EMG characteristic
-    //   // return service.getCharacteristic('d5060205-a904-deb9-4748-2c7f4a124842');
-    // })
-    // .then(characteristic => {
-    //   // batteryLevelCharacteristic = characteristic;
-    //   commandCharacteristic = characteristic;
-    //   // console.log(batteryLevelCharacteristic);
-    //   // return characteristic.startNotifications()
-    //
-    //   // var value = new Uint8Array([0x03,1,0x03]); //Array made of type of command (vibrate), 1 for activate and last argument for vibration type.
-    //   // console.log(value);
-    //   // characteristic.writeValue(value);
-    //
-    //   //return new Buffer([0x01,3,emg_mode,imu_mode,classifier_mode]);
-    //
-    //   // var commandValue = new Uint8Array([0x01,3,0x02,0x01,0x01]);
-    //   // commandCharacteristic.writeValue(commandValue); //This tells the Myo to enable the streaming of data.
-    //   // console.log(characteristic.readValue());
-    //   //  batteryLevelCharacteristic.addEventListener('characteristicvaluechanged',
-    //   //      handleBatteryLevelChanged);
-    // })
-    // .then(_ => {
-    //   // console.log('vibrated');
-    //
-    //   // Get other service: IMUData Service
-    //   return standardServer.getPrimaryService('d5060002-a904-deb9-4748-2c7f4a124842');
-    //   // characteristic.addEventListener('characteristicvaluechanged', handleBatteryLevelChanged)
-    // })
-    // .then(newService => {
-    //   //Getting IMUData Characteristic
-    //   return newService.getCharacteristic('d5060402-a904-deb9-4748-2c7f4a124842');
-    // })
-    // .then(char => {
-    //   char.startNotifications().then(res => {
-    //     char.addEventListener('characteristicvaluechanged', handleIMUDataChanged);
-    //   })
-    // })
     .catch(error => {console.log('error',error)})
   }
 
@@ -154,6 +104,7 @@ window.onload = function(){
 
   function handlePoseChanged(event){
     console.log('getting here', event);
+    console.log('getting here', event.target.value.getUint8(0));
   }
 
 
@@ -242,10 +193,9 @@ window.onload = function(){
     })
     .then(char => {
       char.startNotifications().then(res => {
+        console.log('here right??');
         char.addEventListener('characteristicvaluechanged', handlePoseChanged);
       })
     })
   }
-
-
 }
