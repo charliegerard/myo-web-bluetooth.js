660fd1213d85f69f6062bf7abdedb94510a49522
Charlie Gerard <charlie.a.gerard@gmail.com>
2017-04-20 20:06:06 +0100

    refactored a little

--- public/index.js
+++ public/index.js
@@ -1,13 +1,37 @@
 // UUIDs can be generated from here: https://www.guidgenerator.com/online-guid-generator.aspx
 
-const availableData = {
-  controlService: 'd5060001-a904-deb9-4748-2c7f4a124842',
-  commandCharacteristic: 'd5060401-a904-deb9-4748-2c7f4a124842',
-  IMUDataService: 'd5060002-a904-deb9-4748-2c7f4a124842',
-  IMUDataCharacteristic: 'd5060402-a904-deb9-4748-2c7f4a124842',
-  EMGDataService: 'd5060005-a904-deb9-4748-2c7f4a124842',
-  batteryService: 0x180f,
-  batteryLevelCharacteristic: 0x2a19
+const services = {
+  controlService: {
+    name: 'control service',
+    code: 'd5060001-a904-deb9-4748-2c7f4a124842'
+  },
+  imuDataService :{
+    name: 'IMU Data Service',
+    code: 'd5060002-a904-deb9-4748-2c7f4a124842'
+  },
+  emgDataService: {
+    name: 'EMG Data Service',
+    code: 'd5060005-a904-deb9-4748-2c7f4a124842'
+  },
+  batteryService: {
+    name: 'battery service',
+    code: 0x180f
+  }
+}
+
+const characteristics = {
+  commandCharacteristic: {
+    name: 'command characteristic',
+    code: 'd5060401-a904-deb9-4748-2c7f4a124842'
+  },
+  imuDataCharacteristic: {
+    name: 'imu data characteristic',
+    code: 'd5060402-a904-deb9-4748-2c7f4a124842'
+  },
+  batteryLevelCharacteristic: {
+    name: 'battery level characteristic',
+    code: 0x2a19
+  }
 }
 
 window.onload = function(){
@@ -21,11 +45,9 @@ window.onload = function(){
       filters: [
         {name: "Myo"},
         {
-          //Battery Service // IMU Data Service // Control Service // EMG Service
-          services: [0x180f, availableData.IMUDataService, availableData.controlService, availableData.EMGDataService ]
+          services: [services.batteryService.code, services.imuDataService.code, services.controlService.code, services.emgDataService.code ]
         }
-      ],
-      // optionalServices: ['battery_service']
+      ]
     })
     .then(device => {
       console.log('Device discovered', device.name);
@@ -35,8 +57,8 @@ window.onload = function(){
       console.log('server device: '+ Object.keys(server.device));
 
       standardServer = server;
-      // getService([availableData.controlService, availableData.IMUDataService], [availableData.commandCharacteristic, availableData.IMUDataCharacteristic], server);
-      return getService([availableData.batteryService], [availableData.batteryLevelCharacteristic], server);
+      getService([services.controlService, services.imuDataService], [characteristics.commandCharacteristic, characteristics.imuDataCharacteristic], server);
+      // return getService([services.batteryService.code], [characteristics.batteryLevelCharacteristic.code], server);
 
       //Test EMG Data Service
       // return server.getPrimaryService('d5060005-a904-deb9-4748-2c7f4a124842');
@@ -94,13 +116,6 @@ window.onload = function(){
     .catch(error => {console.log('error',error)})
   }
 
-  // function handleValueChanged(event){
-  //   // let level = event.target.value.getUint8(0);
-  //   // console.log('New value is: ', level);
-  //   console.log('here');
-  //   console.log(event.target.value);
-  // }
-
   function handleBatteryLevelChanged(event){
     let batteryLevel = event.target.value.getUint8(0);
     console.log('> Battery Level is ' + batteryLevel + '%');
@@ -124,7 +139,7 @@ window.onload = function(){
     var gyroscopeY = event.target.value.getInt16(16) / 16;
     var gyroscopeZ = event.target.value.getInt16(18) / 16;
 
-    console.log(orientationW);
+    console.log('orientation: ', orientationW);
   }
 
   // var notificationsButton = document.getElementById('start');
@@ -145,23 +160,23 @@ window.onload = function(){
   // }
 
 
-  function getService(services, characteristics, server){
+  function getService(requestedServices, requestedCharacteristics, server){
     standardServer = server;
     // let { controlService, IMUService } = services;
     // let { commandChar, IMUDataChar } = characteristics;
-
-    if(services[0] === availableData.batteryService){
-      getBatteryData(services[0], characteristics, standardServer)
-    } else if( services[0] === availableData.controlService) {
-      getControlService(services, characteristics, standardServer);
+    if(requestedServices[0].code === services.batteryService.code){
+      getBatteryData(requestedServices[0], requestedCharacteristics, standardServer)
+    } else if( requestedServices[0].code == services.controlService.code) {
+      getControlService(requestedServices, requestedCharacteristics, standardServer);
     }
   }
 
-  function getBatteryData(service, characteristics, server){
+  function getBatteryData(service, reqChar, server){
     return server.getPrimaryService(service)
     .then(service => {
-      if(characteristics[0] === availableData.batteryLevelCharacteristic){
-        getBatteryLevel(characteristics[0], service)
+      console.log('getting battery service');
+      if(reqChar[0] === characteristics.batteryLevelCharacteristic.code){
+        getBatteryLevel(reqChar[0], service)
       }
     })
   }
@@ -169,6 +184,7 @@ window.onload = function(){
   function getBatteryLevel(characteristic, service){
     return service.getCharacteristic(characteristic)
     .then(char => {
+      console.log('getting battery level characteristic');
       char.addEventListener('characteristicvaluechanged',handleBatteryLevelChanged);
       return char.readValue();
     })
@@ -181,24 +197,28 @@ window.onload = function(){
     })
   }
 
-  function getControlService(services, characteristics, server){
-    let controlService = services[0];
-    let IMUService = services[1];
-    let commandChar = characteristics[0];
-    let IMUDataChar = characteristics[1];
+  function getControlService(requestedServices, requestedCharacteristics, server){
+    let controlService = requestedServices[0].code;
+    let IMUService = requestedServices[1].code;
+    let commandChar = requestedCharacteristics[0].code;
+    let IMUDataChar = requestedCharacteristics[1].code;
 
     return server.getPrimaryService(controlService)
     .then(service => {
+      console.log('getting service: ', requestedServices[0].name);
       return service.getCharacteristic(commandChar);
     })
     .then(characteristic => {
+      console.log('getting characteristic: ', requestedCharacteristics[0].name);
       let commandValue = new Uint8Array([0x01,3,0x02,0x01,0x01]);
       characteristic.writeValue(commandValue);
     })
     .then(_ => {
+      console.log('getting service: ', requestedServices[1].name);
       return standardServer.getPrimaryService(IMUService)
     })
-    then(newService => {
+    .then(newService => {
+      console.log('getting characteristic: ', requestedCharacteristics[1].name);
       return newService.getCharacteristic(IMUDataChar)
     })
     .then(char => {
