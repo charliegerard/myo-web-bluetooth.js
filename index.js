cdcf1f79798be9caff80956c5e5cbdb71b774967
Charlie Gerard <charlie.a.gerard@gmail.com>
2017-04-28 14:08:45 +0100

    start adding basic 3d model to visualize data

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 window.onload = function(){
 
   let button = document.getElementById("connect");
+  let cube, renderer, scene, camera;
+
+  let rotationX = 0;
+  let rotationY = 0;
 
   button.onclick = function(e){
     var myoController = new MyoWB("Myo");
@@ -11,6 +15,8 @@ window.onload = function(){
 
       let batteryLevel = state.batteryLevel + '%';
       let orientationData = state.orientation;
+      rotationX = orientationData[0];
+      rotationY = orientationData[1];
       let accelerometerData = state.accelerometer;
       let gyroscopeData = state.gyroscope;
 
@@ -19,4 +25,33 @@ window.onload = function(){
       let emgData = state.emgData;
     });
   }
+
+  init();
+  render();
+
+  function init(){
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+    renderer = new THREE.WebGLRenderer({alpha: true});
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.getElementsByClassName('container')[0].appendChild( renderer.domElement );
+
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+  }
+
+  function render(){
+    requestAnimationFrame(render);
+
+    cube.rotation.x += rotationX;
+    cube.rotation.y += rotationY;
+
+    renderer.render(scene, camera);
+  }
+
 }
